3a3cea84ed9daa50ec8fea29de9fbef531c4e884
Chiara Cespi Polisiani <chiaracespipro@gmail.com>
2023-02-21 15:11:19 +0100

    ex 2 j'ai envie de pleurer j'ai reussi

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_21_102151) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_21_104204) do
   create_table "tasks", force: :cascade do |t|
-    t.string "rails"
     t.string "title"
-    t.text "detail"
-    t.string "completed"
-    t.string "false"
+    t.text "details"
+    t.boolean "completed", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
